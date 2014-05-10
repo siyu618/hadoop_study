516ffc024ac52c04b143479f625f6c3185fd5e38
tianzy <tianzy@yahoo-inc.com>
2014-05-10 19:02:36 +0800

    add common constants

---
+++ core/src/main/java/com/mycompany/hadoop_study/utils/Constants.java
@@ -0,0 +1,32 @@
+package com.mycompany.hadoop_study.utils;
+
+/**
+ * Created by tianzy on 4/22/14.
+ */
+
+//TODO, only BAR us used, NO need to have a class.
+public final class Constants {
+
+    public static final String EMPTY_STRING = "";
+    public static final String SPACE = " ";
+    public static final String TAB = "\t";
+    public static final String BAR = "|";
+    public static final String COMMA = ",";
+
+    public static final String CTRL_A = "\u0001";
+    public static final String CTRL_B = "\u0002";
+    public static final String CTRL_C = "\u0003";
+    public static final String CTRL_D = "\u0004";
+    public static final String CTRL_E = "\u0005";
+    public static final String CTRL_F = "\u0006";
+    public static final String CTRL_G = "\u0007";
+    public static final String CTRL_X = "\u0018";
+    public static final String CTRL_Y = "\u0019";
+
+    public static final String UTF_8 = "UTF-8";
+
+    // make it private so users can't call it
+    private Constants() {}
+
+
+}
